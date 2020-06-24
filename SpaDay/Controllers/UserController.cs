eaa5e0f2ebdf321bcaca21780cd6140b8e7e7ab9
Mitch Hiltz <mitch.hiltz@gmail.com>
2020-06-24 15:09:28 -0400

    Studio darn near complete

---
+++ SpaDay/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaDay.Models;
+
+namespace SpaDay.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet("/user/add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost("/user")]
+        public IActionResult SubmitAddUserForm(User newUser, string verify)
+        {
+            ViewBag.username = newUser.Username;
+            if (verify == newUser.Password)
+            {
+                return View("Index");
+            }
+            else
+            {
+                ViewBag.error = "Passwords did not match. Please try again.";
+                return View("Add");
+            }
+        }
+    }
+}
+    
