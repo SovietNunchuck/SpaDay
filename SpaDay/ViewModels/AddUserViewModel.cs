8d4dc5161783a3840038e8e5ef94ccabfcfe9062
Mitch Hiltz <mitch.hiltz@gmail.com>
2020-06-29 12:43:29 -0400

    I think I'm done

---
+++ SpaDay/ViewModels/AddUserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaDay.ViewModels
+{
+    public class AddUserViewModel
+    {
+        [Required(ErrorMessage = "Required field")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Field must be between 5 and 15 characters long.")]
+        public string Username { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        [StringLength(20, MinimumLength = 6, ErrorMessage = "Field must be between 6 and 20 characters long.")]
+        public string Password { get; set; }
+
+        [Required(ErrorMessage = "Required field")]
+        public string VerifyPassword { get; set; }
+    }
+}
